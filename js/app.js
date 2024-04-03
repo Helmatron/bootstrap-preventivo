5f97ce141c03b9c5d2b7d9993b4deb5268ca8854
Helmatron <ema.matro@gmail.com>
2024-04-03 15:13:41 +0200

    Finito!! - Devo solo organizzare meglio il codice per renderlo leggibile

--- js/app.js
+++ js/app.js
@@ -38,83 +38,47 @@ function stampaPrezzo(e) {
     const work = document.getElementById('inputWorksType').value
     console.log(work);
 
+    function prezzoStandard(work) {
+        // SE il tipo di valore selezionato corrisponde al "backend"
+        if (work == 'backend') {
+            // ALLORA la variabile "price" sarà = 20.50€/l'ora (10 ore)
+            return price = 20.50 * 10
+            // console.log(price);
+        } else if (work == 'frontend') {
+            // ALLORA la variabile "price" sarà = 15.30€/l'ora (10 ore)
+            return price = 15.30 * 10
+            // SE il tipo di valore selezionato corrisponde a "analysis"
+        } else if (work == 'analysis') {
+            // ALLORA la variabile "price" sarà = 33.60€/l'ora (10 ore)
+            return price = 33.60 * 10
+        }
+    }
+    let prezzoFinale = prezzoStandard(work)
+    console.log(prezzoFinale);
+
     // CREARE una variabile che raccolga il VALORE del codice promozionale tramite ID dal FORM
-    const promotion = document.getElementById('inputPromotion').value
-    console.log(promotion);
+    let discount = document.getElementById('inputDiscount').value
+    console.log(discount);
 
     // CREARE un ARRAY che raccolga i codici promozionali da interrogare successivamente
-    const promotionCode = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"]
-    console.log(promotionCode);
+    const discountCode = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'] // strings objects
+    // console.log(discountCode);
 
-    // CICLO l'array dei codici promozionali per confrontarlo con il valore inserito dall'utente
-    for (let i = 0; i < promotionCode.length; i++) {
-        const checkCode = promotionCode[i];
-        console.log(checkCode);
+    const isValid = discountCode.includes(discount)
+    console.log(isValid);
 
-
-        //VERIFICO SE il campo dei codici promozionali è stato compilato o no
-        // SE il valore è vuoto
-        if (promotion == '') {
-
-            // ALLORA prosegue la funzione per calcolare il prezzo senza sconto
-            // SE il tipo di valore selezionato corrisponde al "backend"
-            if (work == 'backend') {
-                // ALLORA la variabile "price" sarà = 20.50€/l'ora (10 ore)
-                const price = 20.50 * 10
-                // console.log(price);
-                // CREARE una variabile con ELment method per recuperare lID dal DOM e stamparlo nel paragrafo ocn ID "finalPrice"
-                const priceElement = document.getElementById('finalPrice')
-                priceElement.innerHTML = '&euro; ' + price.toFixed(2)
-
-                // SE il tipo di valore selezionato corrisponde al "frontend"
-            } else if (work == 'frontend') {
-                // ALLORA la variabile "price" sarà = 15.30€/l'ora (10 ore)
-                const price = 15.30 * 10
-                // STAMPO il vaolre nell' HTML
-                const priceElement = document.getElementById('finalPrice')
-                priceElement.innerHTML = '&euro; ' + price.toFixed(2)
-
-                // SE il tipo di valore selezionato corrisponde a "analysis"
-            } else if (work == 'analysis') {
-                // ALLORA la variabile "price" sarà = 33.60€/l'ora (10 ore)
-                const price = 33.60 * 10
-                // STAMPO il vaolre nell' HTML
-                const priceElement = document.getElementById('finalPrice')
-                priceElement.innerHTML = '&euro; ' + price.toFixed(2)
-            }
-
-
-            // ALTRIMENTI SE è stato inserito uno dei codici promozionali
-        } else if (promotion == checkCode) {
-            // ALLORA verifica il lavoro selezionato e calcola i prezzi con lo sconto del 25%
-            if (work == 'backend') {
-
-                const price = (20.50 * 10) * 0.25
-                const priceElement = document.getElementById('finalPrice')
-                priceElement.innerHTML = '&euro; ' + price.toFixed(2)
-
-            } else if (work == 'frontend') {
-
-                const price = (15.30 * 10) * 0.25
-                const priceElement = document.getElementById('finalPrice')
-                priceElement.innerHTML = '&euro; ' + price.toFixed(2)
-
-            } else if (work == 'analysis') {
-
-                const price = (33.60 * 10) * 0.25
-                const priceElement = document.getElementById('finalPrice')
-                priceElement.innerHTML = '&euro; ' + price.toFixed(2)
-            }
-
-
-            // ALTRIMENTI mostra messaggio di avviso per codice sbagliato
-        } else if (promotion !== checkCode) {
-            alert('Codice promozionale non valido')
-        }
-
+    if (discount === '') {
+        prezzoStandard(work)
+    } else if (isValid === true) {
+        prezzoFinale = prezzoStandard(work) * 0.25
+    } else {
+        alert ('error')
     }
+    
+    
+
+    const priceElement = document.getElementById('finalPrice')
+    priceElement.innerHTML = '&euro; ' + prezzoFinale.toFixed(2);
+    console.log(prezzoFinale, typeof prezzoFinale); //number
+    prezzoFinale
 }
-
-
-
-
